25ea55dcc515f34783cdabb636b5de4351bade9c
Chris Wu <cwu0569@gmail.com>
2021-07-01 10:28:15 -0700

    script for launching ec2 db

---
+++ dbConfig.sh
@@ -0,0 +1,3 @@
+chmod 400 products-overview-db.pem
+
+ssh -i "/Users/chriswu/Desktop/HackReactor/Products-Overview-Chris/products-overview-db.pem" ubuntu@ec2-54-193-94-194.us-west-1.compute.amazonaws.com
