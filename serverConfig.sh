e312b8da38459fda540e0f1483e774f8cf24264d
Chris Wu <cwu0569@gmail.com>
2021-07-01 11:40:26 -0700

    added script to start ec2 server

---
+++ serverConfig.sh
@@ -0,0 +1,3 @@
+chmod 400 products-overview-server.pem
+
+ssh -i "/Users/chriswu/Desktop/HackReactor/Products-Overview-Chris/products-overview-server.pem" ubuntu@ec2-13-52-218-233.us-west-1.compute.amazonaws.com
